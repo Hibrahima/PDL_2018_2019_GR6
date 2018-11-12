d42ca1940298d3ec5ab3504b467ffb353f039e7e
ba <abdoulhamideba@gmail.com>
2018-11-12 11:11:34 +0100

    attribut_convertor

--- src/main/java/wikipedia/html/HTMLConverter.java
+++ src/main/java/wikipedia/html/HTMLConverter.java
@@ -1,7 +1,15 @@
 package wikipedia.html;
 
+
+
 import interfaces.Converter;
+import interfaces.FileHandler;
+
+
 
 public class HTMLConverter implements Converter{
+	private HTMLExtractor extractor;
+	private FileHandler filehandler;
+	private char separator;
 
 }
