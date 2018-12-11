d64dca083790e065ce69fec42423509c1f7ac02f
abdoulhamideba <abdoulhamideba@gmail.com>
2018-12-11 11:59:34 +0100

    adding helper Constants class

---
+++ src/main/java/helper/Constants.java
@@ -0,0 +1,15 @@
+package helper;
+
+import java.io.File;
+
+public class Constants {
+	
+	public static String WIKI_URLS_FILE_PATH = "input" + File.separator + "data" + File.separator + "wikiurls.txt";
+	public static String EN_BASE_WIKIPEDIA_URL = "https://en.wikipedia.org/wiki/";
+	public static String FR_BASE_WIKIPEDIA_URL = "https://fr.wikipedia.org/wiki/";
+	public static String HTML_OUTPUT_DIR = "output" + File.separator + "html" + File.separator;
+	public static String HTML_TEST_OUTPUT_DIR = "output" + File.separator + "test-html" + File.separator;
+	public static String CONSOLE_WHITE_COLOR = "\033[0;37m";
+	public static String CONSOLE_RED_COLOR = "\033[0;31m";
+
+}
