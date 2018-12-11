6769d8904bd3d26904fadf338987daee44b619c5
MariamCoulibay <maryekacool.mc@gmail.com>
2018-12-11 12:05:49 +0100

    testComparison_of_SSH_clients test case added - testing sze of the number of extracted tables from a specific url

--- src/test/java/test/html/HTMLExtractorTest.java
+++ src/test/java/test/html/HTMLExtractorTest.java
@@ -1,12 +1,39 @@
 package test.html;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+import java.io.IOException;
+
+import org.jsoup.HttpStatusException;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.select.Elements;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
+
+import helper.Constants;
+import interfaces.Extractor;
+import wikipedia.html.HTMLExtractor;
 
 public class HTMLExtractorTest {
 	
-	@Test
-	public void test() {
-		
+	private Extractor extractor;
+	
+	@BeforeEach
+	public void setup() {
+		extractor = new HTMLExtractor();
 	}
-
+	
+	@Test
+	@DisplayName("Comparison_of_SSH_clients")
+	public void testComparison_of_SSH_clients() throws IOException {
+		Document doc = Jsoup.connect(Constants.EN_BASE_WIKIPEDIA_URL + "Comparison_of_SSH_clients").get();
+		Elements tables = extractor.extractTables(doc, Constants.EN_BASE_WIKIPEDIA_URL + "Comparison_of_SSH_clients");
+		assertEquals(3, tables.size());
+	}
+	
+	
 }
