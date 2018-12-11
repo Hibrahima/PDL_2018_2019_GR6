db39af9417bf6d8ba9b5c13b718407ef98a9818e
MariamCoulibay <maryekacool.mc@gmail.com>
2018-12-11 11:51:33 +0100

    extract tables operation signature changed - doc as parameter - ignoring tables with less column

--- src/main/java/interfaces/Extractor.java
+++ src/main/java/interfaces/Extractor.java
@@ -2,12 +2,16 @@ package interfaces;
 
 import java.io.IOException;
 
+import org.jsoup.HttpStatusException;
+import org.jsoup.nodes.Document;
 import org.jsoup.select.Elements;
+
 
 public interface Extractor {
 	public boolean isWikipediaUrl(String url);
 	public Elements ignoredClasses(String url, Elements tableElements);
-	public Elements extractTables(String url) throws IOException;
+	public Elements extractTables(Document doc, String url) throws IOException, HttpStatusException;
 	public Elements ignoredElements(String url, String tag ,Elements tableElements);
+	public Elements ignoreTablesWithLessRows(String url, Elements tableElements, int numberOfRows);
 	
 }
