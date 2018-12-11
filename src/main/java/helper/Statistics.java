bab868e271ea117dcc2f2851fa2887e90e9c2072
mahamadSylla <mahamadsylla5@gmail.com>
2018-12-11 12:48:59 +0100

    Statistics interface added

---
+++ src/main/java/helper/Statistics.java
@@ -0,0 +1,12 @@
+package helper;
+
+public interface Statistics {
+	
+	public String getUrl();
+	public void setUrl(String url);
+	public int getIgnoredTablesNumber();
+	public void setIgnoredTablesNumber(int ignoredTablesNumber);
+	public int getExtractedTablesNumber();
+	public void setExtractedTablesNumber(int extractedTablesNumber);
+
+}
