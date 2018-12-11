6d8e3dffe7bf10e8f2b342d08444802921819acb
aboulhamideba <abdoulhamideba@gmail.com>
2018-12-11 12:31:36 +0100

    small bugs fixed

--- src/test/java/test/html/HTMLConverterTest.java
+++ src/test/java/test/html/HTMLConverterTest.java
@@ -1,50 +1,63 @@
 package test.html;
 
-import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
-
-import java.io.BufferedReader;
 import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
 
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
-
 import helper.Constants;
 import helper.FileHandlerImpl;
 import interfaces.FileHandler;
 import wikipedia.html.HTMLConverter;
 
 public class HTMLConverterTest {
-	
+
 	private HTMLConverter htmlConverter;
 	private FileHandler fileHandler;
-	
+	private char separator;
+
 	@BeforeEach
 	public void setup() {
-		 this.htmlConverter = new HTMLConverter();
-		 this.fileHandler = new FileHandlerImpl();
+		this.htmlConverter = new HTMLConverter();
+		this.fileHandler = new FileHandlerImpl();
+		separator = ',';
 	}
-	
+
 	@Test
-	//@Disabled
-	public void testConvetToCSV()
-	{
-		htmlConverter.convertToCsv(Constants.BASE_WIKIPEDIA_URL, "Comparison_between_Esperanto_and_Ido", Constants.HTML_TEST_OUTPUT_DIR);
+	@DisplayName("converts a single url' tables into csv files into our test folders")
+	public void testSingleUrlToCSV() throws IOException {
+		assertTrue(new File(Constants.HTML_TEST_OUTPUT_DIR).isDirectory());
+		Document doc = Jsoup.connect(Constants.EN_BASE_WIKIPEDIA_URL + "Comparison_between_Esperanto_and_Ido").get();
+		assertDoesNotThrow(() -> htmlConverter.convertToCsv(doc, Constants.EN_BASE_WIKIPEDIA_URL,
+				"Comparison_between_Esperanto_and_Ido", Constants.HTML_TEST_OUTPUT_DIR));
 		System.out.println();
-		//assertTrue(false);
+
 	}
+
+
+
+	@Test
+	@DisplayName("test converts all files to csv")
+	@Tag("robustness")
+	public void testConvertAllToCSV() {
+		// htmlConverter.convertAllToCSV();
+		assertTrue(new File(Constants.HTML_OUTPUT_DIR).isDirectory());
+		assertDoesNotThrow(() -> htmlConverter.convertAllToCSV());
+	}
+
 	
 	@Test
-	@Disabled
-	public void testConvertAllToCSV()
-	{
-		htmlConverter.convertAllToCSV();
-		//assertTrue(false);
-	}
-	
-	@Test
-	@Disabled
+	@DisplayName("test validy of all csv files")
+	@Tag("robustness")
 	public void testAreCsvFilesValid() {
 		File[] files = null;
 		int counter = 0;
@@ -53,19 +66,18 @@ public class HTMLConverterTest {
 			htmlDirectory = new File(Constants.HTML_OUTPUT_DIR);
 			assertTrue(htmlDirectory.isDirectory());
 			files = htmlDirectory.listFiles();
-			for(File f : files) {
+			for (File f : files) {
 				counter++;
-				System.out.println("current filename = "+f.getName());
+				System.out.println("current filename = " + f.getName());
 				assertTrue(this.fileHandler.isCsvFileValid(f, ','));
 			}
 		} catch (Exception e) {
 			// TODO: handle exception
 			System.out.println(e.getMessage());
 		}
-		
-		System.out.println("counter = "+counter);
-		
+
+		System.out.println("counter = " + counter);
+
 	}
-	
 
 }
