d327d441283b4359313dae88e40f4a848f433ed7
abdoulhamideba <abdoulhamideba@gmail.com>
2018-12-11 11:57:01 +0100

    process td text operation impplemented - url does exist method incorporated to convert to csv - red color for non existing urls

--- src/main/java/wikipedia/html/HTMLConverter.java
+++ src/main/java/wikipedia/html/HTMLConverter.java
@@ -7,7 +7,9 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.regex.Pattern;
-
+import org.jsoup.HttpStatusException;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 import helper.Constants;
@@ -32,6 +34,16 @@ public class HTMLConverter implements Converter {
 		return Pattern.matches(numericRegex, charString);
 	}
 
+	private boolean doesUrlExist(String url) throws IOException {
+		try {
+			Jsoup.connect(url).get();
+			return true;
+		} catch (HttpStatusException e) {
+			System.out.println(Constants.CONSOLE_RED_COLOR+"[" + url + "] n'existe pas!");
+			return false;
+		}
+	}
+
 	@Override
 	public void convertAllToCSV() {
 		File file = null;
@@ -43,11 +55,14 @@ public class HTMLConverter implements Converter {
 			br = new BufferedReader(fr);
 			String url = null;
 			while ((url = br.readLine()) != null) {
-				//System.out.println("wikipedia url : " + Constants.BASE_WIKIPEDIA_URL + url);
-				convertToCsv(Constants.BASE_WIKIPEDIA_URL, url, Constants.HTML_OUTPUT_DIR);
+				if (!doesUrlExist(Constants.EN_BASE_WIKIPEDIA_URL + url))
+					continue;
+
+				Document doc = Jsoup.connect(Constants.EN_BASE_WIKIPEDIA_URL + url).get();
+				convertToCsv(doc, Constants.EN_BASE_WIKIPEDIA_URL, url, Constants.HTML_OUTPUT_DIR);
 			}
 		} catch (Exception e) {
-			//System.out.println("-------------------------------------" + e.getMessage());
+			// System.out.println("-------------------------------------" + e.getMessage());
 			e.printStackTrace();
 		} finally {
 			try {
@@ -61,62 +76,75 @@ public class HTMLConverter implements Converter {
 
 	}
 
-	public void convertToCsv(String baseUrl, String url, String filePath) {
+	public void convertToCsv(Document doc, String baseUrl, String url, String filePath) throws HttpStatusException {
 		List<String> data = new ArrayList<>();
 		String line = "";
 		String filename;
 		StringBuilder currentTdText;
 		int filenameCounter = 1;
 		try {
-
-			Elements tableElements = this.extractor.extractTables(baseUrl + url);
-			for (Element currentTable : tableElements) {
-				Elements currentTableTrs = currentTable.select("tr");
-				for (int i = 0; i < currentTableTrs.size(); i++) {
-					Element currentTr = currentTableTrs.get(i);
-					Elements currentRowTds = currentTr.select("td");
-					for (int j = 0; j < currentRowTds.size(); j++) {
-						currentTdText = new StringBuilder(currentRowTds.get(j).text());
-						for (int k = 0; k < currentTdText.length(); k++) {
-							if (currentTdText.charAt(k) == separator) {
-								if (k > 0) {
-									if (!isNumeric(currentTdText.charAt(k - 1))) {
-										currentTdText.setCharAt(k, ' ');
-										//System.out.println("current td text = " + currentTdText);
-									} else {
-										if ((k + 1) < currentTdText.length()) {
-											if (isNumeric(currentTdText.charAt(k + 1))) {
-												// System.out.println("current char "+currentTdText.charAt(k-1)+" td
-												// text "+currentTdText);
-												currentTdText.setCharAt(k, '.');
-											} else
-												currentTdText.setCharAt(k, ' ');
-										}
-									}
-								}
-							}
+			Elements tableElements = this.extractor.extractTables(doc, baseUrl + url);
+			if (tableElements == null)
+				System.out.println("Oups, smething wrong happened");
+			else {
+				for (Element currentTable : tableElements) {
+					Elements currentTableTrs = currentTable.select("tr");
+					for (int i = 0; i < currentTableTrs.size(); i++) {
+						Element currentTr = currentTableTrs.get(i);
+						Elements currentRowTds = currentTr.select("td");
+						for (int j = 0; j < currentRowTds.size(); j++) {
+							currentTdText = new StringBuilder(currentRowTds.get(j).text());
+							currentTdText = processCurrentTDText(currentTdText);
+							if (j == currentRowTds.size() - 1)
+								line += currentTdText.toString();
+							else
+								line += currentTdText.toString() + separator;
 						}
-						if (j == currentRowTds.size() - 1)
-							line += currentTdText.toString();
-						else
-							line += currentTdText.toString() + separator;
+						if (line != "") {
+							data.add(line);
+							line = "";
+						}
 					}
-					if (line != "") {
-						data.add(line);
-						line = "";
-					}
+					filename = this.filehandler.extractFilenameFromUrl(url, filenameCounter);
+					this.filehandler.write(filePath, filename, data);
+					System.out.println(Constants.CONSOLE_WHITE_COLOR+filename + " has been generated");
+					filenameCounter++;
+					data.clear();
 				}
-				filename = this.filehandler.extractFilenameFromUrl(url, filenameCounter);
-				this.filehandler.write(filePath, filename, data);
-				System.out.println(filename + " has been generated");
-				filenameCounter++;
-				data.clear();
 			}
 
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
 
+	}
+
+	@Override
+	public StringBuilder processCurrentTDText(StringBuilder tdText) {
+		tdText = new StringBuilder(tdText);
+		for (int k = 0; k < tdText.length(); k++) {
+			if (tdText.charAt(k) == separator) {
+				if (k > 0) {
+					if (!isNumeric(tdText.charAt(k - 1))) {
+						tdText.setCharAt(k, ' ');
+						// System.out.println("current td text = " + currentTdText);
+					} else {
+						if ((k + 1) < tdText.length()) {
+							if (isNumeric(tdText.charAt(k + 1))) {
+								// System.out.println("current char "+currentTdText.charAt(k-1)+" td
+								// text "+currentTdText);
+								tdText.setCharAt(k, '.');
+							} else
+								tdText.setCharAt(k, ' ');
+						}
+					}
+				}
+				if(k == tdText.length()-1)
+					tdText.setCharAt(k, separator);
+			}
+		}
+		
+		return tdText;
 	}
 
 }
