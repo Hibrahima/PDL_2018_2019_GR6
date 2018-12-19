a8efd3ee0d299b210b12633ea05c589aa0e3ebca
Hibrahima <haidara99@gmail.com>
2018-12-19 12:15:38 +0100

    - updated extractor

--- src/test/java/wiki/WikiExtractorTest.java
+++ src/test/java/wiki/WikiExtractorTest.java
@@ -2,21 +2,18 @@ package wiki;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
-
 import java.io.IOException;
-
-import org.jsoup.HttpStatusException;
-import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.select.Elements;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
-
 import helper.Constants;
+import helper.UrlNotFoundException;
+import helper.WikiRunner;
 import interfaces.Extractor;
-import wikipedia.html.WikiExtractor;
+import wikipedia.wiki.WikiExtractor;
 
 public class WikiExtractorTest {
 	
@@ -29,26 +26,26 @@ public class WikiExtractorTest {
 	
 	@Test
 	@DisplayName("Comparison_of_SSH_clients")
-	public void testComparison_of_SSH_clients() throws IOException {
-		Document doc = Jsoup.connect(Constants.EN_BASE_WIKIPEDIA_URL + "Comparison_of_SSH_clients").get();
+	public void testComparison_of_SSH_clients() throws IOException, UrlNotFoundException{
+		Document doc = WikiRunner.getDocument("en", "Comparison_of_SSH_clients");
 		Elements tables = extractor.extractTables(doc, Constants.EN_BASE_WIKIPEDIA_URL + "Comparison_of_SSH_clients");
 		assertEquals(3, tables.size());
 	}
 	
 	@Test
-	@DisplayName("Comparison_of_TLS_implementations")
-	public void testComparison_of_TLS_implementations() throws IOException {
-		Document doc = Jsoup.connect(Constants.EN_BASE_WIKIPEDIA_URL + "Comparison_of_TLS_implementations").get();
+	@DisplayName("Comparison_of_TLS_implementations") 
+	public void testComparison_of_TLS_implementations() throws IOException, UrlNotFoundException {
+		Document doc = WikiRunner.getDocument("en", "Comparison_of_TLS_implementations");
 		Elements tables = extractor.extractTables(doc, Constants.EN_BASE_WIKIPEDIA_URL + "Comparison_of_TLS_implementations");
 		assertEquals(18, tables.size());
 	}
 	
 	@Test
 	@DisplayName("Python_(langage)")
-	public void testComparison_of_PythonLangage() throws IOException {
-		Document doc = Jsoup.connect(Constants.FR_BASE_WIKIPEDIA_URL + "Python_(langage)").get();
+	public void testComparison_of_PythonLangage() throws IOException, UrlNotFoundException {
+		Document doc = WikiRunner.getDocument("fr", "Python_(langage)");
 		Elements tables = extractor.extractTables(doc, Constants.FR_BASE_WIKIPEDIA_URL + "Python_(langage)");
-		assertEquals(0, tables.size());
+		assertEquals(4, tables.size());
 	}
 	
 	
@@ -56,22 +53,22 @@ public class WikiExtractorTest {
 	@DisplayName("Comparison_of_Exchange_ActiveSync_clients")
 	@Tag("robustness")
 	public void testComparison_of_Exchange_ActiveSync_clients() throws IOException {
-		assertThrows(HttpStatusException.class, () -> Jsoup.connect(Constants.EN_BASE_WIKIPEDIA_URL + "Comparison_of_Exchange_ActiveSync_clients").get());
+		assertThrows(UrlNotFoundException.class, () -> WikiRunner.getDocument("en", "Comparison_of_Exchange_ActiveSync_clients"));
 	}
 	
 	@Test
 	@DisplayName("Comparison_of_IPv6_application_support")
 	@Tag("robustness")
 	public void testComparison_of_IPv6_application_support() throws IOException {
-		assertThrows(HttpStatusException.class, () -> Jsoup.connect(Constants.EN_BASE_WIKIPEDIA_URL + "Comparison_of_IPv6_application_support").get());
+		assertThrows(UrlNotFoundException.class, () ->  WikiRunner.getDocument("en", "Comparison_of_IPv6_application_support"));
 	}
 	
 	
 	@Test
 	@DisplayName("Comparison_of_audio_player_software)")
 	@Tag("robustness")
-	public void testComparison_of_audio_player_software() throws IOException {
-		Document doc = Jsoup.connect(Constants.EN_BASE_WIKIPEDIA_URL + "Comparison_of_audio_player_software").get();
+	public void testComparison_of_audio_player_software() throws IOException, UrlNotFoundException {
+		Document doc = WikiRunner.getDocument("en", "Comparison_of_audio_player_software");
 		Elements tables = extractor.extractTables(doc, Constants.EN_BASE_WIKIPEDIA_URL + "Comparison_of_audio_player_software");
 		assertEquals(9, tables.size());
 	}
