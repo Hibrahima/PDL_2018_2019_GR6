89b606a61fcd75b89d88264af979d58dd79d6359
Hibrahima <haidara99@gmail.com>
2018-12-19 12:12:59 +0100

    - updated class imports

--- src/main/java/interfaces/FileHandler.java
+++ src/main/java/interfaces/FileHandler.java
@@ -6,8 +6,6 @@ import java.util.List;
 public interface FileHandler {
 	
 	public void write(String filePath, String filename, List<String> data);
-	/*  le parametre  a tenir en compte String fileLocation ,*/
 	public String extractFilenameFromUrl(String url, int number);
-	
 	public boolean isCsvFileValid(File file, char separator);
 }
